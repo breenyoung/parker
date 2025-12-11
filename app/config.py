30e417c46fa491eec512a63024c896a55cb95ba3
Breen Young <breen.young@gmail.com>
2025-12-11 14:47:53 -0400

    remove batch window setting as it's covered in the database version of the setting now

--- app/config.py
+++ app/config.py
@@ -39,9 +39,6 @@ class Settings(BaseSettings):
     # Supported formats
     supported_extensions: list = [".cbz", ".cbr"]
 
-    #batch_window_seconds: int = 600 # 10 mins
-    batch_window_seconds: int = 300
-
     # --- NEW CONFIG STYLE ---
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
